3e6f1bd80687aa7defab325ab5bd053cf838ee41
Gastón Vidal <gnvidal25@gmail.com>
2024-09-02 00:22:23 -0300

    Refactor upload to s3 to use async await

--- lambda/index.js
+++ lambda/index.js
@@ -102,13 +102,8 @@ exports.handler = async (event, context, callback) => {
         logger.info("Page Title:", await page.title());
         logger.info("SVG Output:", output);
 
-        uploadSVGToS3(config.bucketName, output)
-            .then(data => {
-                console.log(`File uploaded successfully. ${data.Location}`);
-            })
-            .catch(err => {
-                console.error(`Failed to upload file: ${err.message}`);
-            });
+        const data = await uploadSVGToS3(config.bucketName, output);
+        console.log(`S3 Upload Data: ${data}`);
 
     } catch (error) {
         logger.info("Error message");
@@ -258,26 +253,33 @@ async function waitForValueChange(page, selectors, iterationCount, timeout, effi
     }, selectors, iterationCount, timeout, efficiency))
 }
 
-const uploadSVGToS3 = (bucketName, svgOutput) => {
+const uploadSVGToS3 = async (bucketName, svgOutput) => {
 
-    const s3 = new AWS.S3({apiVersion: "2006-03-01"});
-    const keyPrefix = 'result/' + randomUUID() + '/'; // Optional: specify a folder in the bucket
+    try {
+        const s3 = new AWS.S3({apiVersion: "2006-03-01"});
+        const keyPrefix = 'result/' + randomUUID() + '/'; // Optional: specify a folder in the bucket
 
-    const tmpPath = path.join('/tmp', '/tizada');
-    fs.mkdirSync(tmpPath, {recursive: true});
-    fs.writeFileSync(path.join(tmpPath, 'result.svg'), svgOutput, 'utf-8');
+        const tmpPath = path.join('/tmp', '/tizada');
+        fs.mkdirSync(tmpPath, {recursive: true});
+        fs.writeFileSync(path.join(tmpPath, 'result.svg'), svgOutput, 'utf-8');
 
-    const filePath = path.join(tmpPath, 'result.svg');
+        const filePath = path.join(tmpPath, 'result.svg');
 
-    const fileName = path.basename(filePath);
-    const fileContent = fs.readFileSync(filePath);
+        const fileName = path.basename(filePath);
+        const fileContent = fs.readFileSync(filePath);
 
-    const params = {
-        Bucket: bucketName,
-        Key: keyPrefix + fileName,
-        Body: fileContent,
-        ContentType: 'image/svg+xml'
-    };
+        const params = {
+            Bucket: bucketName,
+            Key: keyPrefix + fileName,
+            Body: fileContent,
+            ContentType: 'image/svg+xml'
+        };
 
-    return s3.upload(params).promise();
+        const data = await s3.upload(params).promise();
+        console.log(`File uploaded successfully. ${data.Location}`);
+        return data;
+    } catch (error) {
+        console.error('Error uploading to S3:', error);
+        throw error;
+    }
 };
